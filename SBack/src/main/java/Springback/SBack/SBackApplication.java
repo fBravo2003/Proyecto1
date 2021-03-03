07666c865a0c6b2bc2cf2131523d3c3c5bac2df6
Fabian <fabianbravo2003@gmail.com>
2021-03-03 16:08:00 -0400

    Se agrega back de spring SBack

---
+++ SBack/src/main/java/Springback/SBack/SBackApplication.java
@@ -0,0 +1,13 @@
+package Springback.SBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBackApplication.class, args);
+	}
+
+}
---
+++ SBack/src/test/java/Springback/SBack/SBackApplicationTests.java
@@ -0,0 +1,13 @@
+package Springback.SBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
